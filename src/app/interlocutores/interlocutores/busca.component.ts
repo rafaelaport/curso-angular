c7ccd6013ae2855404e283770269c9b047cc3f33
rafaelaport <rife-rafe@hotmail.com>
2019-02-14 01:01:09 -0200

    melhorando codigo de mostrar parametro na tela

--- src/app/interlocutores/interlocutores/busca.component.ts
+++ src/app/interlocutores/interlocutores/busca.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-busca',
@@ -8,11 +9,24 @@ import { ActivatedRoute } from '@angular/router';
 export class BuscaComponent implements OnInit {
 
   nome: string;
+  inscricao: Subscription;
 
   constructor(private route: ActivatedRoute) {
-    this.nome = this.route.snapshot.params['nome'];
+    //this.nome = this.route.snapshot.params['nome'];
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // FAZ MESMA COISA QUE ESTA NO CONSTRUCTOR
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.nome = params['nome'];
+      }
+    );
+   }
+
+   // boa pratica para se descrever da inscrição
+   ngOnDestroy(){
+     this.inscricao.unsubscribe();
+   }
 
 }
